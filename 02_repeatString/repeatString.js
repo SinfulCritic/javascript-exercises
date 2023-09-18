d0da3a804399e6fba0e320eb3bd4baef20d0df79
SinfulCritic <theispoulsen@modtaget.dk>
2023-09-18 17:00:37 +0200

    1st and 2nd excersise done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let content = '';
+    if (num >= 0) {
+        for (x = 0; x < num; x++) {
+            content = content + string;
+        };
+    }
+    else {
+        content = 'ERROR';
+    };
+    return (content);
 };
 
 // Do not edit below this line
