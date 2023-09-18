a2373738e5ed5845e9c1a2fb41b5c06d556dd018
SinfulCritic <theispoulsen@modtaget.dk>
2023-09-18 17:34:52 +0200

    Finished 3rd excersise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    let contentTmp = string.split('');
+    let content = [];
 
+    for (let x = 0; x < contentTmp.length; x++) {
+        content.unshift(contentTmp[x]);
+    }
+
+    let newContent = '';
+
+    for (let x = 0; x < content.length; x++) {
+        newContent = newContent + content[x];
+        console.log(newContent);
+    }
+    return(newContent);
 };
 
+reverseString('hello there buddy')
 // Do not edit below this line
 module.exports = reverseString;
